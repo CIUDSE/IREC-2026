6c324ba3b65f556b9157091c61cb26fedf688828
SebastianMP34 <jose.mendez.peyro@uabc.edu.mx>
2026-04-03 17:41:32 -0700

    Código de aviónica V0.1.3 con SD y LoRa asíncrono
    Actualización de código de aviónica:
    -Se modificó el envío de datos LoRa a modo asíncrono para no bloquear el código.
       - Se agregó la funcion onLoraTxDone que funciona como el callback de que se termino el envío de datos.
      - Se agrego una función timeout en caso de que el callback no funcione.
    
    -Se simplifico el envio de datos en el archivo .ino, ahora envia datos cada vez que esta disponible para enviar. (Es el if !loraEnviando)
    
    -Se modificó el struct de telemetryData cambiando el tipo de dato a int16_t para el acelerometro y el tiempo recibido.
    
    -Se agrego el archivo con funciones para almacenar datos en la SD. El formato es: avdata_[fecha de encendido de la PCB].

---
+++ Aviónica/Avionica_Primera_Etapa/Funciones_SD.cpp
@@ -0,0 +1,70 @@
+/********************************************************************************************************************************************
+Club de Investigación Univesitario de Desarrollo en Sistemas Espaciales
+AVIÓNICA PRIMERA ETAPA - Misión IREC 2026 - Archivo de funciones de barometro
+Código desarrollado por equipo de Aviónica y Estación Terrena
+
+Archivo de funciones para guardar los datos en la memoria SD (Memoria de SD de Teensy 4.1).
+*********************************************************************************************************************************************/
+#include "Avionica_Primera_Etapa.h"
+
+File datalog;
+
+char fileName[40];
+char buffer[200];
+
+int i = 0;
+
+void inicializarSD()
+{
+  //Hacer ifs de deteccion de gps
+  if (!SD.begin(BUILTIN_SDCARD)) 
+  {
+    Serial.println("Error inicializando SD");
+    return;
+  }
+
+  // Crear nombre con fecha (Ejemplo: avdata_20260403_153045.txt)
+  if(Serial8.available())
+    sprintf(fileName, "avdata_%04d%02d%02d_%02d%02d%02d.txt", gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());
+
+  Serial.print("Archivo: "); 
+  Serial.println(fileName);
+
+}
+
+void guardarSD()
+{
+  datalog = SD.open(fileName, FILE_WRITE);
+
+  snprintf(buffer, sizeof(buffer), "%.6f,%.6f,%c,%u,%u,%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+      sensorData.latitud,
+      sensorData.longitud,
+      telemetryData.id,
+      i,
+      telemetryData.indicadorEstadoVuelo,
+      sensorData.satelites,
+      sensorData.latitud,
+      sensorData.longitud,
+      sensorData.presion,
+      sensorData.altura_barometro,
+      sensorData.temperatura,
+      sensorData.velocidad,
+      sensorData.vel_ang_x,
+      sensorData.vel_ang_y,
+      sensorData.vel_ang_z,
+      sensorData.accel_x,
+      sensorData.accel_y,
+      sensorData.accel_z
+    );
+
+    if(datalog)
+    {    
+      datalog.print(buffer);
+      datalog.close();
+    }
+    else
+    {
+      Serial.println("Error abriendo archivo");
+    }
+    i++;
+}
